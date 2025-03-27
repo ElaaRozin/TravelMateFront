41756a9e4d1aa329c73d49b7f3976021983ab315
ElaaRozin <elarozistudy@gmail.com>
2025-03-27 15:37:25 +0200

    changed to vm ports

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -38,7 +38,7 @@ const Register: React.FC = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:3006/api/register', {
+      const response = await fetch('http://35.184.227.186:3006/api/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, username, password }),
